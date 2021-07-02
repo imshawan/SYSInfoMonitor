0b4fadb0da731077831712377655d0b0a09d4716
Shawan Mandal <shawan.sm049@gmail.com>
2021-07-02 12:16:59 +0530

    Changes in Ui placements

--- SYSInfo Monitor/UIForms/Network.Designer.cs
+++ SYSInfo Monitor/UIForms/Network.Designer.cs
@@ -78,7 +78,7 @@ namespace SYSInfo_Monitor.UIForms
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(34, 51);
+            this.label1.Location = new System.Drawing.Point(41, 44);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(312, 32);
             this.label1.TabIndex = 1;
@@ -90,7 +90,7 @@ namespace SYSInfo_Monitor.UIForms
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Segoe UI Semibold", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(67, 111);
+            this.label2.Location = new System.Drawing.Point(71, 104);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(136, 20);
             this.label2.TabIndex = 2;
@@ -100,7 +100,7 @@ namespace SYSInfo_Monitor.UIForms
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Segoe UI Semibold", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(67, 142);
+            this.label3.Location = new System.Drawing.Point(71, 135);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(127, 20);
             this.label3.TabIndex = 2;
@@ -110,7 +110,7 @@ namespace SYSInfo_Monitor.UIForms
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Segoe UI Semibold", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(67, 205);
+            this.label5.Location = new System.Drawing.Point(71, 198);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(84, 20);
             this.label5.TabIndex = 2;
@@ -120,7 +120,7 @@ namespace SYSInfo_Monitor.UIForms
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(209, 111);
+            this.label6.Location = new System.Drawing.Point(213, 104);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(97, 20);
             this.label6.TabIndex = 2;
@@ -130,7 +130,7 @@ namespace SYSInfo_Monitor.UIForms
             // 
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label7.Location = new System.Drawing.Point(209, 142);
+            this.label7.Location = new System.Drawing.Point(213, 135);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(97, 20);
             this.label7.TabIndex = 2;
@@ -140,7 +140,7 @@ namespace SYSInfo_Monitor.UIForms
             // 
             this.label8.AutoSize = true;
             this.label8.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label8.Location = new System.Drawing.Point(209, 205);
+            this.label8.Location = new System.Drawing.Point(213, 198);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(97, 20);
             this.label8.TabIndex = 2;
@@ -150,7 +150,7 @@ namespace SYSInfo_Monitor.UIForms
             // 
             this.label9.AutoSize = true;
             this.label9.Font = new System.Drawing.Font("Segoe UI Semibold", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label9.Location = new System.Drawing.Point(67, 235);
+            this.label9.Location = new System.Drawing.Point(71, 228);
             this.label9.Name = "label9";
             this.label9.Size = new System.Drawing.Size(84, 20);
             this.label9.TabIndex = 2;
@@ -160,7 +160,7 @@ namespace SYSInfo_Monitor.UIForms
             // 
             this.label10.AutoSize = true;
             this.label10.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label10.Location = new System.Drawing.Point(209, 235);
+            this.label10.Location = new System.Drawing.Point(213, 228);
             this.label10.Name = "label10";
             this.label10.Size = new System.Drawing.Size(97, 20);
             this.label10.TabIndex = 2;
@@ -175,7 +175,7 @@ namespace SYSInfo_Monitor.UIForms
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(67, 171);
+            this.label4.Location = new System.Drawing.Point(71, 164);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(0, 20);
             this.label4.TabIndex = 2;
@@ -184,7 +184,7 @@ namespace SYSInfo_Monitor.UIForms
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(338, 35);
+            this.pictureBox1.Location = new System.Drawing.Point(345, 28);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(96, 96);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
@@ -206,9 +206,9 @@ namespace SYSInfo_Monitor.UIForms
             this.groupBox1.Controls.Add(this.label6);
             this.groupBox1.Controls.Add(this.label4);
             this.groupBox1.Location = new System.Drawing.Point(0, 20);
-            this.groupBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(2);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(2);
             this.groupBox1.Size = new System.Drawing.Size(460, 285);
             this.groupBox1.TabIndex = 4;
             this.groupBox1.TabStop = false;
